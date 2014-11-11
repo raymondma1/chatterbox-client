cfeff41c0223edc6185f6873be74da81acecdc40
raymondma1 <raymondxma@gmail.com>
2014-11-10 16:16:59 -0800

    implement send, fetch, clear, and add

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,40 @@
 // YOUR CODE HERE:
 
+var app = {
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: message sent');
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  clearMessages: function() {
+    $('#chats').remove();
+  },
+  addMessage: function(message) {
+    console.log("msg", message);
+    var msg = $('<li>').text(message);
+    $('#chats').append(msg);
+  }
+};
+
