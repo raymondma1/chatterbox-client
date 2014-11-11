bb91a581be512ae715a832be7df672a547604cfe
raymondma1 <raymondxma@gmail.com>
2014-11-10 16:17:56 -0800

    fetch messages on load

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -18,23 +18,41 @@ var app = {
   },
   fetch: function() {
     $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
       dataType: 'json',
       contentType: 'application/json',
       success: function(data) {
-        console.log(data);
+        _.each(data.results,function(message){
+          app.addMessage(message);
+        });
       },
       error: function(data) {
-        console.error('chatterbox: Failed to send message');
+        console.error('chatterbox: Failed to receive message', data);
       }
     });
   },
   clearMessages: function() {
-    $('#chats').remove();
+    $('#chats').empty();
   },
   addMessage: function(message) {
-    console.log("msg", message);
-    var msg = $('<li>').text(message);
+    var username = $('<div class="username">').text(message.username);
+    var text = $('<div>').text(message.text);
+    var msg = $('<div class = "chat">').append(username).append(text);
     $('#chats').append(msg);
+  },
+  addRoom: function(room) {
+    var cRoom = $('<input type="radio" name="room" value="' + room  +'">' + room)
+    $('#roomSelect').append(cRoom);
+  },
+  addFriend: function(username) {
+
+
   }
 };
 
+$(document).ready(function() {
+  app.fetch();
+  $('#chats').on('click','.username',function() {
+    console.log("click event")
+  });
+});
